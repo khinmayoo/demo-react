115bb9ba5063710d087d00985339e2f100aecf09
khinmayoo <goomlay@gmail.com>
2023-09-07 15:52:41 +0630

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState} from 'react';
+import logo from './logo.svg';
+import Another from './Another';
+import './App.css';
+
+function App() {
+      const [count, setCount] = useState(0);
+
+      function decrement() {
+          setCount(prevCount => prevCount - 1);
+      }
+
+    function increment() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+      const someStyle = {
+          background : 'blue',
+          color : 'white',
+          fontSize : '28px',
+          fontWeight : 'bold'
+      };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Another name="KMO" />
+          <div>
+              <span>
+                  {count}
+                  <button onClick={decrement}>-</button>
+                  <button onClick={increment}>+</button>
+              </span>
+          </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          {true &&
+            <p style={someStyle}>{ 4 + 6} </p>
+          }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
